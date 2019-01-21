79dd6d2b1380d32b1d88c0fb39075b9afd35aadb
itymofeev <Iliya.t@tenengroup.com>
2019-01-21 10:14:19 +0300

    Introduced 'ConsoleClient' proj.

---
+++ ExpressionCalculator/ExpressionCalculator.ConsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExpressionCalculator.Service.Interfaces;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors.Generator;
+using Microsoft.ServiceFabric.Actors;
+using System;
+
+namespace ExpressionCalculator.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workerActorEndpoint = ActorNameFormat.GetFabricServiceUri(typeof(IWorkerActor), "ExpressionCalculator");
+            var worker = ActorProxy.Create<IWorkerActor>(ActorId.CreateRandom(), workerActorEndpoint);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
